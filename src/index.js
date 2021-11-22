e02d1d6225cca03b8a5266ef0a1a2abecf58b78f
Rafael Estrada <rafaelestrada@comcast.net>
2021-11-21 22:16:09 -0800

    added css,and link pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import reactDom from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+
+  document.getElementById("root")
+);
