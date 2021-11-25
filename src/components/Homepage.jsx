45ba4c1e583f0cb0eb37bf68980c8c554bbe7343
Rafael Estrada <rafaelestrada@comcast.net>
2021-11-24 21:29:59 -0800

    fixed footer

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Typography,Row,Col,Statistic } from 'antd';
+import {Link} from 'react-router-dom';
 
 const Homepage = () => {
     return (
-        <div>
-            Homepage
-        </div>
+      <>
+      </>
     )
 }
 
