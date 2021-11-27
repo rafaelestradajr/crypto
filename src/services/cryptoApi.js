31d7176f105b3b02a807790a4d37de5c0158ca5e
Rafael Estrada <rafaelestrada@comcast.net>
2021-11-26 21:05:15 -0800

    building api request

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,29 @@
+import create from '@ant-design/icons/lib/components/IconFont';
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query';
+
+
+
+const cryptoApiHeaders={
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '5820da373bmsh060d85d1ff3edefp13e661jsndea78bcfd3fc'
+} 
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const createRequest =(url) => ({url,headers:cryptoApiHeaders})
+
+
+export const cryptoApi = createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints:(builder) =>({
+        getCryptos:builder.query({
+            query:()=> 'exchanges'
+        })
+    })
+    });  
+    
+    
+    
+    
+    
+ 
