666bbfcc8dd2eeb41e017460f1b5f08d73105b1c
Rafael Estrada <rafaelestrada@comcast.net>
2021-11-27 21:31:52 -0800

    removed api key

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,7 +5,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders={
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '5820da373bmsh060d85d1ff3edefp13e661jsndea78bcfd3fc'
+    //'x-rapidapi-key': '5820da373bmsh060d85d1ff3edefp13e661jsndea78bcfd3fc'
 } 
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
