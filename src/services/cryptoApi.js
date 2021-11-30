ca7d3f3f71afa212c675ed32a5cf71fe43b87363
Rafael Estrada <rafaelestrada@comcast.net>
2021-11-29 20:16:43 -0800

    fixed api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
   "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-  "x-rapidapi-key": ,
+  "x-rapidapi-key": 
 };
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
